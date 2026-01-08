948b47ab578cbb6e0080aae9ea340878b4fafaef
saad076 <saadabban76@gmail.com>
2026-01-08 23:39:03 +0530

    adding new types to escrow

--- programs/escrow/src/states/mod.rs
+++ programs/escrow/src/states/mod.rs
@@ -9,6 +9,7 @@ pub struct Escrow {
     pub mint_b: Pubkey, // public key of the token that the owner wants in return of mint_a
     pub receive: u64, // the amount of mint_b tokens expected in return of mint_a tokens
     pub bump: u8,
+    pub username: String,
     // NOTE : notice we didnt declare the variable to store the amount of mint_a that owner wants to sell just like mint_b tokens in "receive"
     // thats because the vault account will eventually store that information for us
 }
